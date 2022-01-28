b1b6e2a6789742f7948cf824c095c36b200b80fe
Alaina Thompson <athompson9@hawk.iit.edu>
2022-01-28 12:37:00 -0600

    Initial Commit

---
+++ Shared/Sum_SeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sum_SeriesApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Sum_SeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
